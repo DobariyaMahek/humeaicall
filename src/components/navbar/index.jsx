58dee07bab4327069870a738bbc70b01c12e3ece
mahek <mahek282.rejoice@gmail.com>
2024-08-02 10:29:15 +0530

    logout issue

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -104,10 +104,11 @@ const Navbar = (props) => {
                   className="mt-3 text-sm font-medium  transition duration-150 ease-out  hover:ease-in"
                   onClick={() => {
                     localStorage.clear();
+                    window.location.href = "/";
                     window.location.reload();
                   }}
                 >
-                  Log Out
+                  Logout
                 </a>
               </div>
             </div>
